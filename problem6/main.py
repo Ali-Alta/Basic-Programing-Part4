0c51376e8b1ce8d22154d15a5f5297d420b97db4
alisofyansaputra <Buffware4@gmail.com>
2024-02-28 00:54:55 +0700

    initial.main.py

--- problem6/main.py
+++ problem6/main.py
@@ -1,7 +1,24 @@
 def full_prima(N):
     # your code here
     return ''
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+    for digit in str(N):
+        if not is_prime(int(digit)):
+            return False
+    
+    return True
 
+def full_prima(N):
+    
+    if not is_prime(N):
+        return False
+    
 if __name__ == '__main__':
     print(full_prima(2)) # True
     print(full_prima(3)) # True
