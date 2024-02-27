a55d1937db7890f4911b211251b2ee2607b5d920
alisofyansaputra <Buffware4@gmail.com>
2024-02-28 00:46:49 +0700

    response.main.py

--- problem4/main.py
+++ problem4/main.py
@@ -1,10 +1,12 @@
 def palindrome(input_string):
+    cleaned_string = ''.join(input_string.split()).lower()
+    return cleaned_string == cleaned_string[::-1]
+def palindrome(input_string):
+    # your code here
     return 'error response'
-
-
 if __name__ == '__main__':
-    print(palindrome("civic")) # True
-    print(palindrome("katak")) # True
-    print(palindrome("kasur rusak")) # True
-    print(palindrome("kupu-kupu")) # False
-    print(palindrome("lion")) # False
+    print(palindrome("civic"))        # True
+    print(palindrome("katak"))        # True
+    print(palindrome("kasur rusak"))  # True
+    print(palindrome("kupu-kupu"))    # False
+    print(palindrome("lion"))         # False
